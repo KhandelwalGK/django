f7b03e1416bcbc6963a1b01f2601462f49b8644d
saurav <64076504+KhandelwalGK@users.noreply.github.com>
2021-07-15 12:44:59 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,4 +2,6 @@
 isExistApp=$(docker ps -q -f name=dcontainer)
 if [[ -n  $isExistApp ]]; then
     docker rm -f dcontainer
+    docker tag dimage:latest 558443770462.dkr.ecr.us-east-2.amazonaws.com/dimage:latest
+    docker push 558443770462.dkr.ecr.us-east-2.amazonaws.com/dimage:latest
 fi
