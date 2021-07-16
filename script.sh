e2a48d608afca71c9b14ba30103a111e7ff9f276
saurav <64076504+KhandelwalGK@users.noreply.github.com>
2021-07-16 18:28:23 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,4 +2,4 @@
 aws ecr get-login --no-include-email --region us-east-2 > pass.sh
 chmod +x pass.sh
 ./pass.sh
-aws  ecs update-service --cluster django-git-cluster --service django-service --desired-count 1 --region us-east-2
+aws  ecs update-service --cluster django-git-cluster --service django-service --desired-count 0 --region us-east-2
