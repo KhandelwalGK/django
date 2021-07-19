ac92c63cbe88c8bf014a498cc7c291abe341c8cd
saurav <64076504+KhandelwalGK@users.noreply.github.com>
2021-07-19 11:45:04 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-cd ../../../../../..
+cd ../../../../../../
 cd ~/.aws/
 aws  ecs update-service --cluster django-git-cluster --service django-service --desired-count 0 --region us-east-2
 
