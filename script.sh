21fca9119a78b1ad8e073408f9a82cc2c9a37d86
saurav <64076504+KhandelwalGK@users.noreply.github.com>
2021-07-19 11:19:27 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 aws ecr get-login --no-include-email --region us-east-2 > pass.sh
-s=$(aws  ecs update-service --cluster django-git-cluster --service django-service --desired-count 1 --region us-east-2)
+s=$(aws  ecs update-service --cluster django-git-cluster --service django-service --desired-count 0 --region us-east-2)
 echo $s
 chmod +x pass.sh
 ./pass.sh
