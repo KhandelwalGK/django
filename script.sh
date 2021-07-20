0ecd19b22a935f60a12637b8aabe34951847bf64
saurav <64076504+KhandelwalGK@users.noreply.github.com>
2021-07-20 11:25:54 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+isExistApp=$(docker ps -q -f name=dcontainer)
+if [[ -n  $isExistApp ]]; then
+    echo "pushing image"
+     aws ecr get-login --no-include-email --region us-east-2 > pass.sh
+     chmod +x pass.sh
+     ./pass.sh
+    docker tag dimage:latest 558443770462.dkr.ecr.us-east-2.amazonaws.com/dimage:latest
+    docker push 558443770462.dkr.ecr.us-east-2.amazonaws.com/dimage:latest
+fi
