47c321794fc7a26d12585bb65a0f448d3c740981
saurav <64076504+KhandelwalGK@users.noreply.github.com>
2021-07-21 11:23:38 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,6 +5,7 @@ if [[ -n  $isExistApp ]]; then
      aws ecr get-login --no-include-email --region us-east-2 > pass.sh
      chmod +x pass.sh
      ./pass.sh
+      aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 558443770462.dkr.ecr.us-east-2.amazonaws.com
     docker tag dimage:latest 558443770462.dkr.ecr.us-east-2.amazonaws.com/dimage:latest
     docker push 558443770462.dkr.ecr.us-east-2.amazonaws.com/dimage:latest
 fi
