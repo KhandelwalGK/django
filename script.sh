a4d15466c34cdc71202a7356504077a7276a6ad7
saurav <64076504+KhandelwalGK@users.noreply.github.com>
2021-07-21 12:03:08 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,8 @@
 isExistApp=$(docker ps -q -f name=dcontainer)
 if [[ -n  $isExistApp ]]; then
     echo "pushing image"
-     
+     aws ecr get-login --no-include-email --region us-east-2 > pass.sh
+     cat pass.sh
      chmod +x pass.sh
      ./pass.sh
    
